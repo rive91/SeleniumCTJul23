bccb783a8f90ca844f5540f4124e8a5bd5855357
rive <rive@MSI>
2023-07-22 11:03:04 -0600

    First Commit

---
+++ SelJul23/src/main/java/prueba/SeleniumTest.java
@@ -0,0 +1,17 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Selenium");
+		String exePath = "./driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
