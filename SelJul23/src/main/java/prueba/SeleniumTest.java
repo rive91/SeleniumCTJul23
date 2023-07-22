6bb05bdfb0deeda89115038f65667ad39cc25e6d
rive <rive@MSI>
2023-07-22 11:32:09 -0600

    finalisazo el cambio para la task TST-1234

--- SelJul23/src/main/java/prueba/SeleniumTest.java
+++ SelJul23/src/main/java/prueba/SeleniumTest.java
@@ -11,7 +11,7 @@ public class SeleniumTest {
 		String exePath = "./driver/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", exePath);
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://www.facebook.com");
 	}
 
 }
